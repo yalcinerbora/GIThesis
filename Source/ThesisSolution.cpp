115c7552119ea905c6a96b75654168f42d880a51
Bora Yalçıner <yalciner.bora@metu.edu.tr>
2015-08-14 14:07:24 +0300

    testing aabb intersection

--- Source/ThesisSolution.cpp
+++ Source/ThesisSolution.cpp
@@ -31,7 +31,7 @@ ThesisSolution::ThesisSolution(DeferredRenderer& dRenderer, const IEVector3& int
 	, bar(nullptr)
 	, relativeTransformBuffer(1)
 	, voxelScene(CVoxelGrid{{intialCamPos.getX() - (512.0f * 0.5f * 0.5f), intialCamPos.getY() - (512.0f * 0.5f * 0.5f), intialCamPos.getZ() - (512.0f * 0.5f *  0.5f)},
-							0.5f, {256, 256, 256}, 9})
+							0.5f, {0, 0, 0}, 9})
 {
 	voxelCacheUsageSize.AddData(0);
 	voxelScene.LinkDeferredRendererBuffers(dRenderer.GetGBuffer().getDepthGLView(),
